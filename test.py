79a55b52864b665e463ff30580cac7c71d5110b5
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-09-13 21:20:48 +0200

    Learn about function declarations and loops in python.

--- test.py
+++ test.py
@@ -1,8 +1,21 @@
 
-
-# first python code
-name = raw_input("What do you want?")
-print("Sounds good:", name)
-
 # calculating using python: 
-example_string = 'doesn\'t' 
+example_string = 'doesn\'t'
+mult = 4 * 'asdf'
+print(mult[-1:])
+
+for i in range(9):
+    print(i)
+
+
+def fib(n):
+    "docstring"
+    # write fibonacci series up to n
+    a, b = 0, 1
+    while a<n:
+        print(a)
+        a, b = b, a+b
+    print()
+
+
+fib(15)
