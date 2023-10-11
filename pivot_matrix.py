42189669a48f8c027bab62d43cf33ee9a9c6468f
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-10-11 11:51:19 +0200

    Solve pivoting heart problem.

---
+++ pivot_matrix.py
@@ -0,0 +1,17 @@
+
+# Copy the previous grid value, and write code that pivots it. 
+        
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+for y in range(6):
+    for x in range(9):
+        print(grid[x][y], end = ' ')
+    print()
