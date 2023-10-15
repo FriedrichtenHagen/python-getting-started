f3a18019f5f8b2183b271af1b776765de36756cb
Friedrich ten Hagen <friedrich.ten.hagen@googlemail.com>
2023-10-15 14:48:57 +0200

    Solve tests.

--- randomquiz.py
+++ randomquiz.py
@@ -1,5 +1,6 @@
 
 import random
+import os
 # The quiz data. Keys are states and values are their capitals.
 capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
    'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
@@ -17,23 +18,38 @@ capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
    'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont':
    'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
 
-header = 'Please write down the matching capital to each state.'
+header = 'Please write down the matching capital to each state. \n \n'
+answer_letters = ['A', 'B', 'C', 'D']
 
-for i in range(35):
+def createTests():
+    for i in range(3):
+        only_states = list(capitals.keys())
+        random.shuffle(only_states)
+        only_capitals = list(capitals.values())
+        random.shuffle(only_capitals)
 
-    shuffled_capitals = random.shuffle(capitals)
-    only_states = list(shuffled_capitals.keys())
-    only_capitals = list(shuffled_capitals.values())    
+        # write test
+        test = open(f'/Users/friedrichtenhagen/coding/python-getting-started/tests/test{i}.txt', 'w')   
+        test.write(header)
+        for state in only_states:
+            test.write(f'What is the capital of {state}? ')
+            test.write('\n')
 
-    # write test
-    test = open('/Users/friedrichtenhagen/coding/python-getting-started/tests/test{i}', 'w')   
-    test.write(header)
-    for t in shuffled_capitals:
-        test.write('{t}: ')
-    test.close()
-    # write answer sheet
-    answer_sheet = open('/Users/friedrichtenhagen/coding/python-getting-started/answers/answer_sheet{i}', 'w')   
-    answer_sheet.write(header)
-    for t in shuffled_capitals:
-        answer_sheet.write('{t}: ')
-    answer_sheet.close()
+            # get 1 right answer
+            right_answer = capitals[f'{state}']
+            # and 3 wrong answers
+            capitals_right_answer_removed = only_capitals.copy()
+            del capitals_right_answer_removed[capitals_right_answer_removed.index(right_answer)]
+            wrong_answers = random.sample(capitals_right_answer_removed, 3)
+            # sum all 4 answers
+            four_answers = [right_answer] + wrong_answers
+            random.shuffle(four_answers)
+
+            for f in range(4):
+                test.write(f'{answer_letters[f]}: {four_answers[f]}')
+                test.write('\n')
+            test.write('\n')
+        test.close()
+
+
+createTests()
